958f6323b9b5a08264979c639816ba4608d33441
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-21 23:49:24 +0530

    Add part time & wage

---
+++ Employwagescomputeuc3.java
@@ -0,0 +1,35 @@
+package computeemploywages;
+
+public class Employwagescomputeuc3 {
+	
+		  int IS_PART_TIME=1;
+		  int IS_FULL_TIME =2;
+		  int EMP_RATE_PER_HOUR=20;
+
+		public void empWage(){
+		  int emphrs=0;
+		  int empwage=0;
+
+		  double empcheck = Math.floor(Math.random() * 10) % 3;
+
+		  if (empcheck == IS_FULL_TIME)
+		  {  emphrs=8;
+		  
+		  }
+		  else if (empcheck == IS_PART_TIME)
+		  { emphrs=4;
+		  
+		  }
+
+		     empwage=emphrs * EMP_RATE_PER_HOUR;
+
+		     System.out.println("EMPWAGE="+empwage);
+		    }
+		  public static void main(String [] args){
+		    Employwagescomputeuc3 e=new Employwagescomputeuc3();
+		    e.empWage();
+		   }
+		 }
+
+
+
